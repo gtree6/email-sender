262d4c2f6fefe1ba79a5d66bdfc7adc55cd6ef08
Georgi Trifonov <georgi.trifonov@mentormate.com>
2016-03-17 19:41:57 +0200

    Finally managed to configue mail server on Ubuntu to actually send emails to gmail. Emails received with form info, From header, cc header

--- sender.php
+++ sender.php
@@ -1,11 +1,24 @@
 <?php
+session_start();
+
+$message;
+
 //checking if posted inputs are empty and validate if all is ok
 //require going back if missing field
     if (!empty($_POST['questionField'] && !empty($_POST['nameField'] && !empty($_POST['mailField'])))) {
         $question = validate_input($_POST['questionField']);
         $name = validate_input($_POST['nameField']);
-        $mail = validate_input($_POST['nameField']);
+        $mail = validate_input($_POST['mailField']);
+        
         echo $question. " from " . $name . " with email: " . $mail;
+        $message = "Hello, \r\nthis is an automated e-mail from my php email-sending script. "
+                . " It contains my question to you, my name and my contact e-mail.";
+        $message .= "\r\nMy question is: " . $question;
+        $message .= "\r\nSent from: " . $name;
+        $message .= "\r\nE-mail: " . $mail;
+        send_email($message, $mail);
+        
+        echo "<br>".$message;
     } else {
         echo "You have missed a required input field. Please, fill it in before submitting.";
         echo "<br><b><a href='javascript:history.go(-1)'>Go back</a></b>";
@@ -17,5 +30,14 @@
         $input = stripslashes($input);
         $input = htmlspecialchars($input);
         return $input;
+    }
+    
+    function send_email($msg, $mail) {
+        $email = mail("georgi.trifonov@mentormate.com", "Test", $msg, "From:" . $mail);
+        if($email){
+            echo "<h2>Thank you for using our mail form</h2>";
+          }else{
+            echo "<h2>Mail sending failed.</h2>"; 
+          }
     }
 ?>
